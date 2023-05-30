4f1a3740281c8e833f1196caa006d36da05e0191
ThePowerdinoDeluxe990 <118630450+ThePowerdinoDeluxe990@users.noreply.github.com>
2023-05-30 12:11:45 +0200

    Create index.ts

---
+++ index.ts
@@ -0,0 +1,9 @@
+let numero: number = 3
+
+if (numero % 2 == 0 && numero%3==0) {
+  console.log("Son los dos")
+} else if (numero % 3 == 0) {
+  console.log("No es par")
+}else if(numero %2==0 ){
+  console.log("Es par")
+}
